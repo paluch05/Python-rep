e7f9ceeebc49d6a1aa7fe2fbd4760be3e72e0592
paluch05 <annapaluch05@gmail.com>
2020-12-01 19:53:30 +0100

    Task 20

--- Task20/Task20.py
+++ Task20/Task20.py
@@ -1,29 +1,53 @@
 import time
-from typing import List, Callable
+from datetime import datetime
 
 
-def first_sorting(lst: List):
-    lst.sort()
+def bubble_sort(list):
+    for iter_num in range(len(list) - 1, 0, -1):
+        for idx in range(iter_num):
+            if list[idx] > list[idx + 1]:
+                temp = list[idx]
+                list[idx] = list[idx + 1]
+                list[idx + 1] = temp
 
 
-def second_sorting(lst: List):
-    lst.sort(reverse=True)
+def insertion_sort(list):
+    for i in range(1, len(list)):
+        j = i - 1
+        nxt_element = list[i]
+
+        while (list[j] > nxt_element) and (j >= 0):
+            list[j + 1] = list[j]
+            j = j - 1
+        list[j + 1] = nxt_element
 
 
-def measure_sort_time(sorting_function: Callable[[List], None], lst: List):
-    start = time.time()
-    sorting_function(lst)
-    end = time.time()
+def is_sorted(lst):
+    previous = lst[0]
+    for element in lst[1:]:
+        if element < previous:
+            return False
+        previous = element
+
+    return True
+
+
+def measure_sort_time(sorting_function, list):
+    start = datetime.now()
+    sorting_function(list)
+    end = datetime.now()
     dif = end - start
     return dif
 
 
 if __name__ == '__main__':
-    my_arr = [1, 5, 43, 7, 432, 675, 3, 2]
+    my_list = [1, 5, 43, 7, 432, 675, 3, 2]
 
-    first_run_time = measure_sort_time(first_sorting, my_arr)
-    print(f"First sorting took {first_run_time:0.9f} s")
+    first = measure_sort_time(bubble_sort, my_list)
+    print(f"First sorting took {first.total_seconds():0.20f} s")
 
-    second_run_time = measure_sort_time(second_sorting, my_arr)
-    print(f"Second sorting took {second_run_time:0.9f} s")
+    second = measure_sort_time(insertion_sort, my_list)
+    print(f"Second sorting took {second.total_seconds():0.20f} s")
+
+    print(is_sorted(my_list))
 
